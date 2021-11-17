193c67227822d7c4accd10067928d5be74c71ee3
cdbrown0702 <connorbrown@umass.edu>
2021-11-17 15:13:42 -0500

    test test

--- client/scripts/report.js
+++ client/scripts/report.js
@@ -7,7 +7,7 @@ const campus    = L.latLng(42.38922, -72.52650),
 // Sets access token and initializes map, sets boundarys and zoom
 mapboxgl.accessToken = 'pk.eyJ1IjoiY2Ricm93bjA3MDIiLCJhIjoiY2t2anRnOWI0MDQ3djJ1cW5sY2tnNjVkNCJ9.JII6kT21_W_G7O_Lvpww2g';
 const map = new mapboxgl.Map({
-  container: 'map', // container ID
+  container: 'map-small', // container ID
   style: 'mapbox://styles/mapbox/streets-v11', // style URL
   center: campus, // starting position [lng, lat]
   zoom: 16, // starting zoom
@@ -15,9 +15,11 @@ const map = new mapboxgl.Map({
   maxBounds: mapBounds
 });
 
+$('mod').on('shown.bs.modal', function() {
+  map.resize();
+});
 
-//------
-document.getElementById('sub').addEventListener('click', () => {
+document.getElementById('map-small').addEventListener('click', () => {
   let coor = new mapboxgl.Marker({ color: 'black', draggable=true }).setLngLat(campus).addTo(map);
   coor.getLngLat()
 });
