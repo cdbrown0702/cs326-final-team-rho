bc81665847d4046209caedf81a3d8a8777802b58
cdbrown0702 <connorbrown@umass.edu>
2021-12-02 16:24:07 -0500

    tesT

--- server.js
+++ server.js
@@ -4,23 +4,24 @@ const MongoClient = require('mongodb').MongoClient;
 const uri = process.env.MONGO_URL;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-async function connect() {
-    try {
-        await client.connect();
-        console.log(await client.db("Reports").collection("Submission").find({}).toArray());
-        console.log(await client.db("Users").collection("UserList").find({}).toArray());
-    } catch (err) {
-        console.error(err);
-    }
-}
-async function close(){
-    await client.close();
-}
+// async function connect() {
+//     try {
+//         await client.connect();
+//         console.log(await client.db("Reports").collection("Submission").find({}).toArray());
+//         console.log(await client.db("Users").collection("UserList").find({}).toArray());
+//     } catch (err) {
+//         console.error(err);
+//     }
+// }
+// async function close(){
+//     await client.close();
+// }
 
 async function getUsers() {
     try {
-        let u = client.db("Users").collection("UserList");
-        let ret = await u.find({}).toArray();
+        await client.connect();
+        let ret = await client.db("Users").collection("UserList").find({}).toArray();
+        await client.close();
         return ret;
     } catch (err) { console.error(err); }
 }
