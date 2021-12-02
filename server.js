e71c5ea5238d4fb851c164123020f5d61eefa377
cdbrown0702 <connorbrown@umass.edu>
2021-12-02 15:58:22 -0500

    test

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function connect() {
     try {
         await client.connect();
+        console.log(await client.db("Reports").collection("Submission").toArray());
     } catch (err) {
         console.error(err);
     }
