01cfee69933330740a730477eb22c4380fa55666
cdbrown0702 <connorbrown@umass.edu>
2021-12-10 14:03:12 -0500

    adjusted script local

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ const strat = new LocalStrategy(
 );
 
 // password encryption method
-const minicrypt = require('scripts/miniCrypt.js');
+const minicrypt = require('/scripts/miniCrypt.js');
 const mc = new minicrypt();
 
 // Passport setup
